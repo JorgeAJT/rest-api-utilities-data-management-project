cc859133633214eb70d211a384a60bd31da9b18e
JorgeAJT <jorgeajtrueba@gmail.com>
2024-12-06 17:49:35 +0100

    created logger.py to setup any logger and db_connection.py to connect with the external database

---
+++ src/utils/db_connection.py
@@ -0,0 +1,15 @@
+import psycopg2
+from .logger import setup_logger
+
+logger = setup_logger("database")
+
+def db_connection() -> psycopg2.extensions.connection:
+    logger.info("Connecting with the db ...")
+
+    conn = psycopg2.connect(
+        dbname='postgres',
+        user='postgres',
+        password='1234',
+    )
+    logger.info("Successful connection!")
+    return conn
---
+++ src/utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+DEFAULT_LOG_LEVEL = logging.INFO
+DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+def setup_logger(name: str, level: int = DEFAULT_LOG_LEVEL, fmt: str = DEFAULT_LOG_FORMAT) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    # Create handler and formatter
+    handler = logging.StreamHandler()  # Send logs to the console
+    handler.setFormatter(logging.Formatter(fmt))
+
+    # Avoid adding duplicate handlers
+    if not logger.handlers:
+        logger.addHandler(handler)  # Associate the handler to the logger
+
+    return logger
