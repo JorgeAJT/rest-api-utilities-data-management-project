661f245e997755f371ed68677f95433b49f42dfd
JorgeAJT <jorgeajtrueba@gmail.com>
2024-12-11 13:14:40 +0100

    changed all methods clearly, eliminated some try and except and more secure

--- src/mandate_data/mandate_data_get.py
+++ src/mandate_data/mandate_data_get.py
@@ -5,58 +5,50 @@ from src.models import Response
 
 logger = setup_logger('mandate-data-get')
 
-try:
-    conn = db_connection()
-    mandate_data_get_router = APIRouter()
+mandate_data_get_router = APIRouter()
 
-    @mandate_data_get_router.get('/mandate_data/{business_partner_id}', response_model=Response)
-    async def get_mandate_data_by_path_params(business_partner_id: str):
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        try:
-            cursor.execute('SELECT * FROM mandate_data WHERE business_partner_id = %s', (business_partner_id,))
-            value = cursor.fetchall()
+@mandate_data_get_router.get('/mandate_data/{business_partner_id}', response_model=Response)
+async def get_mandate_data_by_path_params(business_partner_id: str):
+    with db_connection() as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+            try:
+                cursor.execute('SELECT * FROM mandate_data WHERE business_partner_id = %s', (business_partner_id,))
+                value = cursor.fetchall()
 
-            if not value:
-                logger.warning(f"No data found for business_partner_id: {business_partner_id}")
-                return Response(status_code=404, message="business_partner_id not found in any mandate_data row")
+                if not value:
+                    logger.warning(f"No data found for business_partner_id: {business_partner_id}")
+                    return Response(status_code=404, message="business_partner_id not found in any mandate_data row")
 
-            logger.info(f"Data successfully retrieved for business_partner_id: {business_partner_id}")
-            return Response(status_code=200, message={"mandate_data": value})
-        except Exception as e:
-            logger.error(f"Error executing query: {e}")
-            return Response(status_code=500, message="An internal error occurred while GET query in mandate_data")
-        finally:
-            cursor.close()
+                logger.info(f"Data successfully retrieved for business_partner_id: {business_partner_id}")
+                return Response(status_code=200, message={"mandate_data": value})
+            except Exception as e:
+                logger.error(f"Error executing query: {e}")
+                return Response(status_code=500, message="An internal error occurred while GET query in mandate_data")
 
-    @mandate_data_get_router.get('/mandate_data/', response_model=Response)
-    async def get_mandate_data_by_query_params(
-            business_partner_id: str,
-            mandate_status: str,
-            collection_frequency: str = None):
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        try:
-            if collection_frequency:
-                query = ('SELECT * FROM mandate_data WHERE business_partner_id = %s AND mandate_status = %s AND '
-                         'collection_frequency = %s')
-                params = (business_partner_id, mandate_status, collection_frequency)
-            else:
-                query = 'SELECT * FROM mandate_data WHERE business_partner_id = %s AND mandate_status = %s'
-                params = (business_partner_id, mandate_status)
-            cursor.execute(query, params)
-            value = cursor.fetchall()
+@mandate_data_get_router.get('/mandate_data/', response_model=Response)
+async def get_mandate_data_by_query_params(
+        business_partner_id: str,
+        mandate_status: str,
+        collection_frequency: str = None):
+    with db_connection() as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+            try:
+                if collection_frequency:
+                    query = ('SELECT * FROM mandate_data WHERE business_partner_id = %s AND mandate_status = %s AND '
+                             'collection_frequency = %s')
+                    params = (business_partner_id, mandate_status, collection_frequency)
+                else:
+                    query = 'SELECT * FROM mandate_data WHERE business_partner_id = %s AND mandate_status = %s'
+                    params = (business_partner_id, mandate_status)
+                cursor.execute(query, params)
+                value = cursor.fetchall()
 
-            if not value:
-                logger.warning(f"No data found for business_partner_id: {business_partner_id}")
-                return Response(status_code=404, message="mandate_data row not found")
+                if not value:
+                    logger.warning(f"No data found for business_partner_id: {business_partner_id}")
+                    return Response(status_code=404, message="mandate_data row not found")
 
-            logger.info(f"Data successfully retrieved")
-            return Response(status_code=200, message={"mandate_data": value})
-        except Exception as e:
-            logger.error(f"Error executing query: {e}")
-            return Response(status_code=500, message="An internal error occurred while GET query in mandate_data")
-        finally:
-            cursor.close()
-
-except Exception as e:
-    logger.error(f"Error: {e}")
-    raise e
+                logger.info(f"Data successfully retrieved")
+                return Response(status_code=200, message={"mandate_data": value})
+            except Exception as e:
+                logger.error(f"Error executing query: {e}")
+                return Response(status_code=500, message="An internal error occurred while GET query in mandate_data")
--- src/mandate_data/mandate_data_post.py
+++ src/mandate_data/mandate_data_post.py
@@ -4,31 +4,24 @@ from src.models import Response, MandateData
 
 logger = setup_logger("mandate-data-post")
 
-try:
-    conn = db_connection()
-    mandate_data_post_router = APIRouter()
+mandate_data_post_router = APIRouter()
 
-    @mandate_data_post_router.post('/mandate_data/', response_model=Response)
-    async def post_mandate_data(mandate_data: MandateData):
-        cursor = conn.cursor()
-        try:
-            values_tuple = tuple(mandate_data.dict().values())
-            cursor.execute("""
-                INSERT INTO mandate_data (
-                    mandate_id, business_partner_id, brand, mandate_status,
-                    collection_frequency, row_update_datetime, row_create_datetime,
-                    changed_by, collection_type, metering_consent
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """, values_tuple)
-            conn.commit()
-            logger.info(f"Successfully inserted into mandate_data: {mandate_data.dict()}")
-            return Response(status_code=201, message={"mandate_data": mandate_data.dict()})
-        except Exception as e:
-            logger.error(f"Error executing query: {e}")
-            return Response(status_code=400, message="mandate_data body wasn't correct")
-        finally:
-            cursor.close()
-
-except Exception as e:
-    logger.error(f"Error: {e}")
-    raise e
+@mandate_data_post_router.post('/mandate_data/', response_model=Response)
+async def post_mandate_data(mandate_data: MandateData):
+    with db_connection() as conn:
+        with conn.cursor() as cursor:
+            try:
+                values_tuple = tuple(mandate_data.dict().values())
+                cursor.execute("""
+                    INSERT INTO mandate_data (
+                        mandate_id, business_partner_id, brand, mandate_status,
+                        collection_frequency, row_update_datetime, row_create_datetime,
+                        changed_by, collection_type, metering_consent
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, values_tuple)
+                conn.commit()
+                logger.info(f"Successfully inserted into mandate_data: {mandate_data.dict()}")
+                return Response(status_code=201, message={"mandate_data": mandate_data.dict()})
+            except Exception as e:
+                logger.error(f"Error executing query: {e}")
+                return Response(status_code=400, message="mandate_data body wasn't correct")
