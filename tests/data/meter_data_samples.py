b82b4e3e985659f8be5d354c7b00514350618efb
JorgeAJT <jorgeajtrueba@gmail.com>
2025-04-09 18:31:12 +0200

    added samples in an independent file to use in every test

---
+++ tests/data/meter_data_samples.py
@@ -0,0 +1,37 @@
+
+METER_DATA_SINGLE = {
+        "meter_number": "00000000003",
+        "connection_ean_code": "871694840223121111",
+        "business_partner_id": "010000007",
+        "brand": "ES",
+        "grid_company_code": "8716948000003",
+        "oda_code": "8712423026766",
+        "smart_collectable": "1",
+        "sjv1": 1313.0,
+        "sjv2": 982.0,
+        "installation": "0801859418",
+        "division": "01",
+        "move_out_date": "9999-12-31T00:00:00",
+        "row_create_datetime": "2024-08-09T07:38:07",
+        "move_in_date": "2015-12-10T00:00:00"
+    }
+
+METER_DATA_LIST = [
+        {
+                "meter_data_id": 2,
+                "meter_number": "000000005912273146",
+                "connection_ean_code": "871694840015271748",
+                "business_partner_id": "0100000022",
+                "brand": "ES",
+                "grid_company_code": "8716948000003",
+                "oda_code": "8712423026766",
+                "smart_collectable": "1",
+                "sjv1": 301.0,
+                "sjv2": None,
+                "installation": "0801225660",
+                "division": "02",
+                "move_out_date": "2024-10-27T00:00:00",
+                "row_create_datetime": "2024-08-09T07:38:07",
+                "move_in_date": "2022-01-21T00:00:00"
+        }
+]
